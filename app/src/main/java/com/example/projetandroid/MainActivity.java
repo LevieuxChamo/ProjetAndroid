23201b964bf1fac22b7f1a3708769a20af35fc70
LevieuxChamo <martin.theo2001@gmail.com>
2022-11-08 15:03:11 +0100

    Recorder commit

--- app/src/main/java/com/example/projetandroid/MainActivity.java
+++ app/src/main/java/com/example/projetandroid/MainActivity.java
@@ -2,15 +2,59 @@ package com.example.projetandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button play, stop, record;
+    private MediaRecorder myAudioRecorder;
+    private String outputFile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //test
-        //WTF LA MISE A JOUR LA
+
+        play = (Button) findViewById(R.id.play);
+        stop = (Button) findViewById(R.id.stop);
+        record = (Button) findViewById(R.id.record);
+
+        stop.setEnabled(false);
+        play.setEnabled(false);
+
+        String outputFile = Environment.getExternalStorageDirectory().getAbsolutePath() + "/recording.3gp";
+
+        MediaRecorder myAudioRecorder = new MediaRecorder();
+        myAudioRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        myAudioRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        myAudioRecorder.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
+        myAudioRecorder.setOutputFile(outputFile);
+
+        record.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try{
+                    myAudioRecorder.prepare();
+                    myAudioRecorder.start();
+                }
+                catch (IllegalStateException ise){
+                    //Android Catch
+                } catch (IOException ioe){
+                    //Input/Output catch
+                }
+
+                record.setEnabled(false);
+                stop.setEnabled(true);
+
+                Toast.makeText(getApplicationContext(), "Recording started", Toast.LENGTH_LONG);
+            }
+        });
     }
 }
